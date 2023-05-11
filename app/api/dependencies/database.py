8b19e693a35dc47ce4465ef510d1338b9308849c
Jorilla Abdullaev <jorilla.abdullaev@protonmail.com>
2023-05-12 01:36:40 +0500

    feat: make use of Annotated in FastAPI dependencies

--- app/api/dependencies/database.py
+++ app/api/dependencies/database.py
@@ -1,5 +1,6 @@
+from typing import Annotated
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from db.session import async_session
 
 
@@ -9,3 +10,6 @@ async def get_db_session() -> AsyncSession:
     """
     async with async_session() as session:
         yield session
+
+
+DbSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
--- app/schemas/pagination.py
+++ app/schemas/pagination.py
@@ -1,4 +1,11 @@
+from typing import Annotated
+from fastapi import Depends
+
+
 class LimitOffsetPaginationParams:
     def __init__(self, limit: int = 20, offset: int = 0):
         self.limit = limit
         self.offset = offset
+
+
+PaginationDep = Annotated[LimitOffsetPaginationParams, Depends(LimitOffsetPaginationParams)]
